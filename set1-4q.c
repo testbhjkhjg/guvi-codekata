7fa837da13de8e8413ea9c1f224d69b14859bea0
vigneshkv23 <vigneshkv23@gmail.com>
2017-08-12 15:05:11 +0530

    Add files via upload

--- set1-4q.c
+++ set1-4q.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    char c;
-    printf("Enter a character: ");
-    scanf("%c",&c);
+    char num;
+    printf("Enter an character: ");
+    scanf("%c",&num);
 
-    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-        printf("%c is an alphabet.",c);
+    if( (num>='a' && num<='z') || (num>='A' && num<='Z'))
+        printf("%c Alphabet.",num);
     else
-        printf("%c is not an alphabet.",c);
+        printf("%c Not a Alphabet.",num);
 
     return 0;
 }
