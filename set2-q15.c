074bd481d6f7c1809a37276d657081d83280e3d4
vigneshkv23 <vigneshkv23@gmail.com>
2017-08-13 12:28:06 +0530

    Add files via upload

---
+++ set2-q15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n,m;
+    printf("Enter the number between two numbers\n");
+    scanf("%d%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+    	if(i%2!=0)
+    	{
+    		
+    	}
+    	else
+    	{
+    		printf("%d\n",i);
+    	}
+    }
+    return 0;
+}    
